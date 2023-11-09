f9b174a64851b98b67a99fbc11f11db2cf8c76dc
Ali Ebrahimi Pourasad <ali_pourasad@yahoo.de>
2023-11-09 16:11:43 +0100

    Initial Commit

---
+++ UXLLM/UXLLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  UXLLMApp.swift
+//  UXLLM
+//
+//  Created by Ali Ebrahimi Pourasad on 09.11.23.
+//
+
+import SwiftUI
+
+@main
+struct UXLLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
