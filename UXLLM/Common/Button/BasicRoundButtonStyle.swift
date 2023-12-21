d1d9c4419e647844d0b0811deb773a68cab31b9e
Ali Ebrahimi Pourasad <ali_pourasad@yahoo.de>
2023-12-21 10:43:11 +0100

    Adjust BasicRoundButtonStyle

--- UXLLM/Common/Button/BasicRoundButtonStyle.swift
+++ UXLLM/Common/Button/BasicRoundButtonStyle.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct BasicRoundButtonStyle: ButtonStyle {
     
-    static let dimension: CGFloat = 100
+    static let dimension: CGFloat = 84
     private let isLoading: Bool
     
     init(isLoading: Bool) {
@@ -24,13 +24,29 @@ struct BasicRoundButtonStyle: ButtonStyle {
             .frame(width: Self.dimension,
                    height: Self.dimension)
             .background(
-                Color("BackgroundGradient2")
+                Color("BasicRoundButtonColor")
+            )
+            .overlay(
+                Circle().stroke(Color("BasicRoundButtonColorStroke"), lineWidth: 8)
             )
             .clipShape(
                 Circle()
             )
-            .shadow(color: .black.opacity(0.3), radius: 5.0, y: 2.0)
+            .opacity(isLoading ? 0.6 : 1.0)
+            .blur(radius: isLoading ? 2.0 : 0.0)
+            .shadow(color: .black.opacity(0.1), radius: 4.0, y: 2.0)
             .scaleEffect(configuration.isPressed ? 0.95 : 1)
             .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
     }
+}
+
+#Preview {
+    Button {
+        
+    } label: {
+        Text("Start")
+    }
+    .buttonStyle(BasicRoundButtonStyle(isLoading: false))
+    .frame(width: 200, height: 200)
+    .background( InputBackgroundView() )
 }
